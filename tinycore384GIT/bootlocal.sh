13f676278c24255a751af753bc593cff7bc3ea7e
evagelos <evagelosvar@yahoo.com>
2016-06-13 13:17:48 +0300

    ok

--- tinycore384GIT/bootlocal.sh
+++ tinycore384GIT/bootlocal.sh
@@ -1,14 +1,35 @@
 #!/bin/sh
-#sudo tce-load -wi rdesktop.tcz
 
-#sudo sh /opt/cm.txt
+#=====================
+#Load Rdesktop
+#=====================
+su - tc -c 'tce-load -wi rdesktop.tcz'
+
+
+#=====================
+#Configure REMOTE DISPLAY for X for Xvesa & Set Xvesa SCREEN ANALYSIS 800x600x16 compatible for most monitors
+#=====================
+VGA="800x600x16"
+sudo sed -i -e 's/1024x768x32/'$VGA'/' /home/tc/.xsession
+sudo sed -i -e 's/-nolisten/-ac/' /home/tc/.xsession
+sudo sed -i -e 's/tcp//' /home/tc/.xsession
 
 
 
+su - tc -c 'startx'
 
-su - tc -c 'sh /tmp/xsr'
-#su - tc -c 'sh /tmp/menu'
-#su - tc -c 'sh /opt/cm.txt'
 
-#sh /tmp/menu
+
+#=====================
+#Loop for main script
+#=====================
+count=0
+while [ "$count" -lt 60 ]; 
+do
+aterm -g 50x50+10+10 -e sh -c 'sh /tmp/xsr'
+sleep 1
+count=$((count + 1))
+done
+
+
 
