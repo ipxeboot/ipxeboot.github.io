317acbd34693305bdcad51ac3ab4464f76a7ca4c
evagelosvar <evagelosvar@yahoo.com>
2016-12-23 13:38:35 +0200

    Update pcon.sh

--- tools/pcon.sh
+++ tools/pcon.sh
@@ -4,6 +4,8 @@ echo evagelosvar |  pcloudcc-2.0.1-Linux/bin/pcloudcc -u evagelosvar@gmail.com -
 sleep 5
 rsync -avzh --delete "pCloudDrive/uploads/" "www/"
 sudo chmod a+x www/master-scripts/*
+sudo chmod a+x www/tools/*
 #cp pCloudDrive/uploads/tools/cron.sh cron.sh
+#www/tools/cron.sh
 
 #sh cron.sh > /tmp/out
